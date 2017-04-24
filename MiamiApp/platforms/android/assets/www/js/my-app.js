ddbdc88d9ec21f6ea9fecf783fe9d7924248214d
SamHausfeld <hausfesa@miamioh.edu>
2017-04-24 22:30:34 +0000

    creating blank.html for poster template image

--- MiamiApp/platforms/android/assets/www/js/my-app.js
+++ MiamiApp/platforms/android/assets/www/js/my-app.js
@@ -38,6 +38,7 @@ $$('.notification-custom').on('click', function () {
     });
 });
 
+
 var mySearchbar = myApp.searchbar('.searchbar', {
 		    searchList: '.list-block-search',
 			    searchIn: '.item-title'
